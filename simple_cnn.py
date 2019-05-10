c7cc26ce7fe39ecc307e9d1e672d9690a39bb2d8
Jonny Sparrenhök <Jonnysp@kth.se>
2019-05-10 15:34:41 +0200

    train

--- simple_cnn.py
+++ simple_cnn.py
@@ -54,7 +54,7 @@ datagen = dict(
 train_datagen = ImageDataGenerator(preprocessing_function=preprocess_input, **datagen)
 
 
-train_generator = train_datagen.flow_from_directory('./dataset/',
+train_generator = train_datagen.flow_from_directory('./dataset/train',
                                                  target_size=(224,224),
                                                  color_mode='rgb',
                                                  batch_size=32,
@@ -62,7 +62,7 @@ train_generator = train_datagen.flow_from_directory('./dataset/',
                                                  shuffle=True,
 												 subset='training')
 
-validation_generator = train_datagen.flow_from_directory('./dataset/',
+validation_generator = train_datagen.flow_from_directory('./dataset/train',
                                                  target_size=(224,224),
                                                  color_mode='rgb',
                                                  batch_size=train_generator.batch_size,
--- transfer-example.py
+++ transfer-example.py
@@ -77,7 +77,7 @@ datagen = dict(
 train_datagen = ImageDataGenerator(preprocessing_function=preprocess_input, **datagen)
 
 
-train_generator = train_datagen.flow_from_directory('./dataset/',
+train_generator = train_datagen.flow_from_directory('./dataset/train',
                                                  target_size=(224,224),
                                                  color_mode='rgb',
                                                  batch_size=32,
@@ -85,7 +85,7 @@ train_generator = train_datagen.flow_from_directory('./dataset/',
                                                  shuffle=True,
 												 subset='training')
 
-validation_generator = train_datagen.flow_from_directory('./dataset/',
+validation_generator = train_datagen.flow_from_directory('./dataset/train',
                                                  target_size=(224,224),
                                                  color_mode='rgb',
                                                  batch_size=train_generator.batch_size,
